533ba5a265f443b721e235d4b4018059ae39d8ca
Thien Phuong <Thienphuong2580@gmail.com>
2025-10-25 00:12:36 +0200

    Fixed bug 1

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,12 +1,14 @@
 let number = Math.floor(Math.random() * 15) + 1;
 
-for(let i = number; i > 0; i++);
-    if(i % 3 === 0) {
-        console.log("Fizz");
+for(let i = 1; i <= number; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
     } else if(i % 5 === 0) {
         console.log("Buzz");
-    } else if(i % 3 === 0 && i % 5 === 0){
-        console.log("FizzBuzz");
+    } else if(i % 3 === 0){
+        console.log("Fizz");
     } else {
         console.log(i);
     }
+}
+
